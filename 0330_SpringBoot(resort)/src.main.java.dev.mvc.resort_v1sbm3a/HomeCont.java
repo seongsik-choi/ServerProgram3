bda4899ffdb5b2b941102f3535468bb3059f04e2
seongsik-choi <rlawlsdk1225@gmail.com>
2021-03-31 09:10:38 +0900

    .java

---
+++ 0330_SpringBoot(resort)/src.main.java.dev.mvc.resort_v1sbm3a/HomeCont.java
@@ -0,0 +1,25 @@
+/*
+0330
+ */
+package dev.mvc.resort_v1sbm3a;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeCont {
+  public HomeCont() {
+    System.out.println("--> HomeCont created.");
+  }
+
+  // http://localhost:9091
+  @RequestMapping(value = {"/", "/index.do"}, method = RequestMethod.GET)
+  public ModelAndView home() {
+    ModelAndView mav = new ModelAndView();
+    mav.setViewName("/index");  // /WEB-INF/views/index.jsp
+    
+    return mav;
+  }
+}
