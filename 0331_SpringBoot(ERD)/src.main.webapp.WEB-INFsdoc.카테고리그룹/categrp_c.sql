58da50902a928a06016318bc1ec71a5ccea36f66
seongsik-choi <rlawlsdk1225@gmail.com>
2021-03-31 16:22:43 +0900

    .sql

---
+++ 0331_SpringBoot(ERD)/src.main.webapp.WEB-INFsdoc.카테고리그룹/categrp_c.sql
@@ -0,0 +1,71 @@
+/**********************************/
+/* Table Name: 카테고리 그룹 */
+/**********************************/
+DROP TABLE categrp;
+
+CREATE TABLE categrp(
+		categrpno                     		NUMBER(10)		 NOT NULL		 PRIMARY KEY,
+		name                          		VARCHAR2(50)		 NOT NULL,
+		seqno                         		NUMBER(7)		 DEFAULT 0		 NOT NULL,
+		visible                       		CHAR(1)		 DEFAULT 'Y'		 NOT NULL,
+		rdate                         		DATE		 NOT NULL
+);
+
+COMMENT ON TABLE categrp is '카테고리 그룹';
+COMMENT ON COLUMN categrp.categrpno is '카테고리 그룹 번호';
+COMMENT ON COLUMN categrp.name is '이름';
+COMMENT ON COLUMN categrp.seqno is '출력 순서';
+COMMENT ON COLUMN categrp.visible is '출력 모드';
+COMMENT ON COLUMN categrp.rdate is '그룹 생성일';
+
+DROP SEQUENCE memo_seq;
+
+CREATE SEQUENCE memo_seq
+  START WITH 1           -- 시작 번호
+  INCREMENT BY 1       -- 증가값
+  MAXVALUE 9999999999  -- 최대값: 9999999999 --> NUMBER(10) 대응
+  CACHE 2                  -- 2번은 메모리에서만 계산
+  NOCYCLE;                 -- 다시 1부터 생성되는 것을 방지
+
+/**********************************/
+/* Table Name: 카테고리 */
+/**********************************/
+CREATE TABLE cate(
+		cateno                        		NUMBER(10)		 NOT NULL		 PRIMARY KEY,
+		categrpno                     		NUMBER(10)		 NULL ,
+  FOREIGN KEY (categrpno) REFERENCES categrp (categrpno)
+);
+
+COMMENT ON TABLE cate is '카테고리';
+COMMENT ON COLUMN cate.cateno is '카테고리 번호';
+COMMENT ON COLUMN cate.categrpno is '카테고리 그룹 번호';
+
+
+/**********************************/
+/* Table Name: 회원 */
+/**********************************/
+CREATE TABLE member(
+		memberno                      		NUMBER(10)		 NOT NULL		 PRIMARY KEY
+);
+
+COMMENT ON TABLE member is '회원';
+COMMENT ON COLUMN member.memberno is '회원 번호';
+
+
+/**********************************/
+/* Table Name: 컨텐츠 */
+/**********************************/
+CREATE TABLE contents(
+		contentsno                    		NUMBER(10)		 NOT NULL		 PRIMARY KEY,
+		cateno                        		NUMBER(10)		 NULL ,
+		memberno                      		NUMBER(10)		 NULL ,
+  FOREIGN KEY (cateno) REFERENCES cate (cateno),
+  FOREIGN KEY (memberno) REFERENCES member (memberno)
+);
+
+COMMENT ON TABLE contents is '컨텐츠';
+COMMENT ON COLUMN contents.contentsno is '컨텐츠 번호';
+COMMENT ON COLUMN contents.cateno is '카테고리 번호';
+COMMENT ON COLUMN contents.memberno is '회원 번호';
+
+
