d66ab81dd398f12552c9ab8b85bed84c913186e1
seongsik-choi <rlawlsdk1225@gmail.com>
2021-04-02 14:21:57 +0900

    .java

--- resort_v1sbm3a/src/main/java/dev/mvc/resort_v1sbm3a/ResortV1sbm3aApplication.java
+++ resort_v1sbm3a/src/main/java/dev/mvc/resort_v1sbm3a/ResortV1sbm3aApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"dev.mvc.resort_v1sbm3a"})
+@ComponentScan(basePackages = {"dev.mvc"})
 public class ResortV1sbm3aApplication {
 
 	public static void main(String[] args) {
