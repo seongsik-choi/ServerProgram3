99f5e857bd339ba2d8b75686cd34121b2594d6cb
seongsik-choi <rlawlsdk1225@gmail.com>
2021-04-03 19:00:08 +0900

    .java

--- resort_v1sbm3a/src/main/java/dev/mvc/categrp/CategrpCont.java
+++ resort_v1sbm3a/src/main/java/dev/mvc/categrp/CategrpCont.java
@@ -43,4 +43,23 @@ public class CategrpCont {
  }
  
  
+  //http://localhost:9091/categrp/create.do
+  /**
+   * 등록 처리
+   * @param categrpVO
+   * @return
+   */
+ @RequestMapping(value="/categrp/create.do", method=RequestMethod.POST)
+  public ModelAndView create(CategrpVO categrpVO) { // categrpVO 자동생성 Form -> VO 클래스로 자동저장
+  // CategrpVO categrpVO <FORM> 태그의 값으로 자동 생성됨.
+  // request.setAttribute("categrpVO", categrpVO); 자동 실행
+    
+    ModelAndView mav = new ModelAndView();
+    
+    int cnt = this.categrpProc.create(categrpVO); // 등록 처리(인터페이스 호출, 클래스를 할당받았었음)
+    mav.addObject("cnt", cnt); // request에 저장, request.setAttribute("cnt", cnt)  
+    mav.setViewName("/categrp/create_msg"); // /webapp/WEB-INF/views/categrp/create_msg.jsp
+    return mav; // forward
+  }
+ 
 }
